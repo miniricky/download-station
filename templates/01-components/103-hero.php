2265f6d6f6959bebe29367a73cb3a8473d343cc9
miniricky <mini.ricky@hotmail.com>
2025-03-01 13:23:47 -0700

    Added hero component.

---
+++ templates/01-components/103-hero.php
@@ -0,0 +1,8 @@
+<div class="section-row bg-purple-dark" id="hero">
+  <div class="container-fluid">
+    <h1>Download Station</h1>
+    <p>Welcome to your centralized anime download hub, where you can explore our extensive collection from AnimeFLV and other popular sources. Access and manage your favorite anime series download episodes directly to your desktop or Synology NAS. Whether you're building a personal library or setting up your media server, we provide a seamless downloading experience with direct downloads and Synology Download Station integration. Simple, efficient, and organized: everything you need in one place.</p>
+  </div>
+
+  <svg viewBox="0 0 1920 60" aria-hidden="true"><path fill="#0b0c2a" d="M-153.5,85.5a4002.033,4002.033,0,0,1,658-71c262.854-6.5,431.675,15.372,600,27,257.356,17.779,624.828,19.31,1089-58v102Z"></path></svg>
+</div>
---
+++ templates/01-components/104-three-cards.php
@@ -0,0 +1,11 @@
+<div class="section-row" id="three-cards">
+  <div class="container-fluid">
+    <div class="animeflv">
+      <div class="image">
+        <a href="./animeflv.php">
+          <img src="./images/sites/animeflv.png" alt="AnimeFLV logo">
+        </a>
+      </div>  
+    </div>
+  </div>
+</div>

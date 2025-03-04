c6a3d11d3648ee247600d758b6f7de7b0cc5635f
miniricky <mini.ricky@hotmail.com>
2025-03-04 12:37:04 -0700

    Updated names to components.

--- animeflv.php
+++ animeflv.php
@@ -352,18 +352,18 @@
       </div>
     </div>
   </div>
+  <div class="toast align-items-center text-bg-danger text-white border-0 position-fixed bottom-0 end-0 m-3" role="alert" aria-live="assertive" aria-atomic="true">
+    <div class="d-flex">
+      <div class="toast-body"></div>
+      <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+    </div>
+  </div>
   <div class="loader-overlay visually-hidden">
     <div class="loader-content">
       <div class="spinner-border text-primary mb-2" role="status">
         <span class="visually-hidden">Loading.. .</span>
       </div>
       <div>Processing download request</div>
-    </div>
-  </div>
-  <div class="toast align-items-center text-bg-danger text-white border-0 position-fixed bottom-0 end-0 m-3" role="alert" aria-live="assertive" aria-atomic="true">
-    <div class="d-flex">
-      <div class="toast-body"></div>
-      <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
     </div>
   </div>
   <footer class="footer mt-auto py-3 text-center bg-purple-dark">
